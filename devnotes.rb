2cb73830e51dded68ba77c433f3017b523764d69
Edu Depetris <edu.depetris.00@gmail.com>
2021-03-06 22:26:48 +1300

    Update version

--- devnotes.rb
+++ devnotes.rb
@@ -1,8 +1,8 @@
 class Devnotes < Formula
   desc "Everything you need to get started with Devnotes"
   homepage "https://github.com/edudepetris/notes-cli"
-  url "https://devnotes-cli.s3.us-east-2.amazonaws.com/devnotes-v0.0.6/devnotes-v0.0.6-darwin-x64.tar.gz"
-  sha256 "da6bd7b3f19a3270c9bba4bb501697eef11539893fa37d340bcdf2bdcaefefd2"
+  url "https://devnotes-cli.s3.us-east-2.amazonaws.com/devnotes-v0.0.7/devnotes-v0.0.7-darwin-x64.tar.gz"
+  sha256 "39507c1f6f29f8205f6b3f684d9a35a144954f89ae66f5c84ee4788235671cf4"
 
   def install
     inreplace "bin/devnotes", /^CLIENT_HOME=/, "export DEVNOTES_OCLIF_CLIENT_HOME=#{lib/"client"}\nCLIENT_HOME="
